700bda662c286359a1e58fc163256fda480b7239
Ryan Deering <ryandeering1@gmail.com>
2019-03-09 21:10:25 +0000

    pushing before making user changes

--- target/scala-2.12/twirl/main/views/html/addEmployee.template.scala
+++ target/scala-2.12/twirl/main/views/html/addEmployee.template.scala
@@ -21,67 +21,66 @@ import play.api.data.Field
 import play.mvc.Http.Context.Implicit._
 import play.data._
 import play.core.j.PlayFormsMagicForJava._
-/*1.2*/import models.employees.Employee
-/*2.2*/import models.employees.User
-/*3.2*/import models.entities.Project;
-/*4.2*/import models.entities.Address;
-/*5.2*/import models.entities.Department;
 
 object addEmployee extends _root_.play.twirl.api.BaseScalaTemplate[play.twirl.api.HtmlFormat.Appendable,_root_.play.twirl.api.Format[play.twirl.api.HtmlFormat.Appendable]](play.twirl.api.HtmlFormat) with _root_.play.twirl.api.Template2[Form[models.employees.Employee],models.employees.User,play.twirl.api.HtmlFormat.Appendable] {
 
   /**/
-  def apply/*7.2*/(employeeForm: Form[models.employees.Employee], user: models.employees.User):play.twirl.api.HtmlFormat.Appendable = {
+  def apply/*2.2*/(employeeForm: Form[models.employees.Employee], user: models.employees.User):play.twirl.api.HtmlFormat.Appendable = {
     _display_ {
       {
-/*8.2*/import helper._
+/*3.2*/import helper._
+/*4.2*/import models.employees.Employee
+/*5.2*/import models.employees.User
+/*6.2*/import models.entities.Project;
+/*7.2*/import models.entities.Address;
+/*8.2*/import models.entities.Department;
 
 
 Seq[Any](format.raw/*9.1*/("""
+"""),_display_(/*10.2*/main("Add Employee", user)/*10.28*/{_display_(Seq[Any](format.raw/*10.29*/("""
+    """),format.raw/*11.5*/("""<p class="lead">Add a new Employee</p>
 
-"""),_display_(/*11.2*/main("Add Employee", user)/*11.28*/{_display_(Seq[Any](format.raw/*11.29*/("""
-    """),format.raw/*12.5*/("""<p class="lead">Add a new Employee</p>
-
-    """),_display_(/*14.6*/form(action=routes.HomeController.addEmployeeSubmit(),
+    """),_display_(/*13.6*/form(action=routes.HomeController.addEmployeeSubmit(),
         'class -> "form-horizontal",
         'role -> "form",
-        'enctype -> "multipart/form-data")/*17.43*/ {_display_(Seq[Any](format.raw/*17.45*/("""
+        'enctype -> "multipart/form-data")/*16.43*/ {_display_(Seq[Any](format.raw/*16.45*/("""
 
 
-        """),_display_(/*20.10*/CSRF/*20.14*/.formField),format.raw/*20.24*/("""
+        """),_display_(/*19.10*/CSRF/*19.14*/.formField),format.raw/*19.24*/("""
 
-        """),_display_(/*22.10*/inputText(employeeForm("email"), '_label -> "id", 'class -> "form-control")),format.raw/*22.85*/("""
+        """),_display_(/*21.10*/inputText(employeeForm("email"), '_label -> "id", 'class -> "form-control")),format.raw/*21.85*/("""
 
-        """),_display_(/*24.10*/inputText(employeeForm("address.address"), '_label -> "Address", 'class -> "form-control")),format.raw/*24.100*/("""
-        """),_display_(/*25.10*/select(
+        """),_display_(/*23.10*/inputText(employeeForm("address.address"), '_label -> "Address", 'class -> "form-control")),format.raw/*23.100*/("""
+        """),_display_(/*24.10*/select(
             employeeForm("department.id"),
             options(Department.options),
             '_label -> "Department", '_default -> "-- Choose a Department --",
             '_showConstraints -> false, 'class -> "form-control"
-        )),format.raw/*30.10*/("""
+        )),format.raw/*29.10*/("""
 
 
-        """),_display_(/*33.10*/inputText(employeeForm("name"), '_label -> "name", 'class -> "form-control")),format.raw/*33.86*/("""
+        """),_display_(/*32.10*/inputText(employeeForm("name"), '_label -> "name", 'class -> "form-control")),format.raw/*32.86*/("""
 
-        """),_display_(/*35.10*/inputText(employeeForm("role").copy(value=Some("employee")), '_label -> "",'hidden -> "hidden")),format.raw/*35.105*/("""
+        """),_display_(/*34.10*/inputText(employeeForm("role").copy(value=Some("employee")), '_label -> "",'hidden -> "hidden")),format.raw/*34.105*/("""
 
-        """),_display_(/*37.10*/inputText(employeeForm("password"), '_label -> "Password", 'class -> "form-control")),format.raw/*37.94*/("""
-
-
-        """),_display_(/*40.10*/for((value, name) <- Project.options) yield /*40.47*/ {_display_(Seq[Any](format.raw/*40.49*/("""
-            """),format.raw/*41.13*/("""<input type="checkbox" name="projSelect[]" value=""""),_display_(/*41.64*/value),format.raw/*41.69*/(""""/>"""),_display_(/*41.73*/name),format.raw/*41.77*/("""<br>
-        """)))}),format.raw/*42.10*/("""
+        """),_display_(/*36.10*/inputText(employeeForm("password"), '_label -> "Password", 'class -> "form-control")),format.raw/*36.94*/("""
 
 
-
+        """),_display_(/*39.10*/for((value, name) <- Project.options) yield /*39.47*/ {_display_(Seq[Any](format.raw/*39.49*/("""
+            """),format.raw/*40.13*/("""<input type="checkbox" name="projSelect[]" value=""""),_display_(/*40.64*/value),format.raw/*40.69*/(""""/>"""),_display_(/*40.73*/name),format.raw/*40.77*/("""<br>
+        """)))}),format.raw/*41.10*/("""
 
 
 
-        """),format.raw/*49.9*/("""<label>Image Upload</label>
+
+
+
+        """),format.raw/*48.9*/("""<label>Image Upload</label>
         <input class="btn-sm btn-default" type="file" name="upload">
         <br><br>
         <div class="actions">
             <input type="submit" value="Add/Update Employee" class="btn btn-primary">
-            <a href=""""),_display_(/*54.23*/routes/*54.29*/.HomeController.index(0)),format.raw/*54.53*/("""">
+            <a href=""""),_display_(/*53.23*/routes/*53.29*/.HomeController.index(0)),format.raw/*53.53*/("""">
                 <button type="button" class="btn btn-warning">Cancel</button>
             </a>
         </div>
@@ -101,11 +100,11 @@ Seq[Any](format.raw/*9.1*/("""
 
               /*
                   -- GENERATED --
-                  DATE: Sat Mar 09 20:31:01 GMT 2019
+                  DATE: Sat Mar 09 21:03:56 GMT 2019
                   SOURCE: /home/ryan/Documents/sdev4CA1/app/views/addEmployee.scala.html
-                  HASH: 331ef5e8dacf64c1859c4888a84d76c0e2b0b20b
-                  MATRIX: 651->1|691->35|727->65|766->98|805->131|1197->168|1346->246|1390->262|1419->265|1454->291|1493->292|1525->297|1596->342|1764->501|1804->503|1843->515|1856->519|1887->529|1925->540|2021->615|2059->626|2171->716|2208->726|2474->971|2513->983|2610->1059|2648->1070|2765->1165|2803->1176|2908->1260|2947->1272|3000->1309|3040->1311|3081->1324|3159->1375|3185->1380|3216->1384|3241->1388|3286->1402|3328->1417|3607->1669|3622->1675|3667->1699
-                  LINES: 24->1|25->2|26->3|27->4|28->5|33->7|36->8|39->9|41->11|41->11|41->11|42->12|44->14|47->17|47->17|50->20|50->20|50->20|52->22|52->22|54->24|54->24|55->25|60->30|63->33|63->33|65->35|65->35|67->37|67->37|70->40|70->40|70->40|71->41|71->41|71->41|71->41|71->41|72->42|79->49|84->54|84->54|84->54
+                  HASH: 76b36abd90aa16a219129838578952b2de4eede8
+                  MATRIX: 1001->2|1150->80|1173->97|1213->131|1249->161|1288->194|1327->227|1390->262|1418->264|1453->290|1492->291|1524->296|1595->341|1763->500|1803->502|1842->514|1855->518|1886->528|1924->539|2020->614|2058->625|2170->715|2207->725|2473->970|2512->982|2609->1058|2647->1069|2764->1164|2802->1175|2907->1259|2946->1271|2999->1308|3039->1310|3080->1323|3158->1374|3184->1379|3215->1383|3240->1387|3285->1401|3327->1416|3606->1668|3621->1674|3666->1698
+                  LINES: 28->2|31->3|32->4|33->5|34->6|35->7|36->8|39->9|40->10|40->10|40->10|41->11|43->13|46->16|46->16|49->19|49->19|49->19|51->21|51->21|53->23|53->23|54->24|59->29|62->32|62->32|64->34|64->34|66->36|66->36|69->39|69->39|69->39|70->40|70->40|70->40|70->40|70->40|71->41|78->48|83->53|83->53|83->53
                   -- GENERATED --
               */
           
--- target/scala-2.12/twirl/main/views/html/employeeProjects.template.scala
+++ target/scala-2.12/twirl/main/views/html/employeeProjects.template.scala
@@ -22,10 +22,10 @@ import play.mvc.Http.Context.Implicit._
 import play.data._
 import play.core.j.PlayFormsMagicForJava._
 
-object employeeProjects extends _root_.play.twirl.api.BaseScalaTemplate[play.twirl.api.HtmlFormat.Appendable,_root_.play.twirl.api.Format[play.twirl.api.HtmlFormat.Appendable]](play.twirl.api.HtmlFormat) with _root_.play.twirl.api.Template3[List[models.entities.Project],models.employees.Employee,models.employees.Employee,play.twirl.api.HtmlFormat.Appendable] {
+object employeeProjects extends _root_.play.twirl.api.BaseScalaTemplate[play.twirl.api.HtmlFormat.Appendable,_root_.play.twirl.api.Format[play.twirl.api.HtmlFormat.Appendable]](play.twirl.api.HtmlFormat) with _root_.play.twirl.api.Template3[List[models.entities.Project],models.employees.Employee,models.employees.User,play.twirl.api.HtmlFormat.Appendable] {
 
   /**/
-  def apply/*1.2*/(projects: List[models.entities.Project], employee: models.employees.Employee, user: models.employees.Employee):play.twirl.api.HtmlFormat.Appendable = {
+  def apply/*1.2*/(projects: List[models.entities.Project], employee: models.employees.Employee, user: models.employees.User):play.twirl.api.HtmlFormat.Appendable = {
     _display_ {
       {
 
@@ -38,19 +38,21 @@ Seq[Any](format.raw/*2.1*/("""
         <table class="table table-bordered table-hover table-condensed">
             <thead>
                 <tr>
+                    <th>Id</th>
                     <th>Name</th>
-                    <th>Description</th>
+
                 </tr>
             </thead>
 
             <tbody>
-            """),_display_(/*16.14*/for(p <- projects) yield /*16.32*/ {_display_(Seq[Any](format.raw/*16.34*/("""
-                """),format.raw/*17.17*/("""<tr>
-                    <td>"""),_display_(/*18.26*/p/*18.27*/.getPname),format.raw/*18.36*/("""</td>
+            """),_display_(/*17.14*/for(p <- projects) yield /*17.32*/ {_display_(Seq[Any](format.raw/*17.34*/("""
+                """),format.raw/*18.17*/("""<tr>
+                    <td>"""),_display_(/*19.26*/p/*19.27*/.getId),format.raw/*19.33*/("""</td>
+                    <td>"""),_display_(/*20.26*/p/*20.27*/.getPname),format.raw/*20.36*/("""</td>
 
                 </tr>
-            """)))}),format.raw/*21.14*/("""
-            """),format.raw/*22.13*/("""</tbody>
+            """)))}),format.raw/*23.14*/("""
+            """),format.raw/*24.13*/("""</tbody>
         </table>
     </div>
 """)))}))
@@ -58,9 +60,9 @@ Seq[Any](format.raw/*2.1*/("""
     }
   }
 
-  def render(projects:List[models.entities.Project],employee:models.employees.Employee,user:models.employees.Employee): play.twirl.api.HtmlFormat.Appendable = apply(projects,employee,user)
+  def render(projects:List[models.entities.Project],employee:models.employees.Employee,user:models.employees.User): play.twirl.api.HtmlFormat.Appendable = apply(projects,employee,user)
 
-  def f:((List[models.entities.Project],models.employees.Employee,models.employees.Employee) => play.twirl.api.HtmlFormat.Appendable) = (projects,employee,user) => apply(projects,employee,user)
+  def f:((List[models.entities.Project],models.employees.Employee,models.employees.User) => play.twirl.api.HtmlFormat.Appendable) = (projects,employee,user) => apply(projects,employee,user)
 
   def ref: this.type = this
 
@@ -69,11 +71,11 @@ Seq[Any](format.raw/*2.1*/("""
 
               /*
                   -- GENERATED --
-                  DATE: Sat Mar 09 20:31:01 GMT 2019
+                  DATE: Sat Mar 09 21:01:17 GMT 2019
                   SOURCE: /home/ryan/Documents/sdev4CA1/app/views/employeeProjects.scala.html
-                  HASH: 157fb20f0f65475bf28977dfb119181f3b2fe306
-                  MATRIX: 1034->1|1239->113|1266->115|1305->146|1344->148|1375->153|1431->183|1447->191|1475->199|1801->498|1835->516|1875->518|1920->535|1977->565|1987->566|2017->575|2090->617|2131->630
-                  LINES: 28->1|33->2|34->3|34->3|34->3|35->4|35->4|35->4|35->4|47->16|47->16|47->16|48->17|49->18|49->18|49->18|52->21|53->22
+                  HASH: 9da5a545ee7704b196fcd6f95d5ce5e3ca8822bf
+                  MATRIX: 1030->1|1231->109|1258->111|1297->142|1336->144|1367->149|1423->179|1439->187|1467->195|1785->486|1819->504|1859->506|1904->523|1961->553|1971->554|1998->560|2056->591|2066->592|2096->601|2169->643|2210->656
+                  LINES: 28->1|33->2|34->3|34->3|34->3|35->4|35->4|35->4|35->4|48->17|48->17|48->17|49->18|50->19|50->19|50->19|51->20|51->20|51->20|54->23|55->24
                   -- GENERATED --
               */
           
